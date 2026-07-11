0cab0a833b6cc880b0b47305a56922ffbf1f3a5c
Drug <cropflre2333@gmail.com>
2026-07-11 18:10:28 +0800

    feat(android): route share intents into Nowen (#220)

--- frontend/android/app/src/main/java/com/nowen/note/MainActivity.java
+++ frontend/android/app/src/main/java/com/nowen/note/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nowen.note;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
@@ -8,6 +9,15 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     registerPlugin(MediaStoreSavePlugin.class);
+    registerPlugin(ShareImportPlugin.class);
     super.onCreate(savedInstanceState);
+    ShareImportPlugin.captureIntent(this, getIntent());
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    setIntent(intent);
+    ShareImportPlugin.captureIntent(this, intent);
   }
 }
