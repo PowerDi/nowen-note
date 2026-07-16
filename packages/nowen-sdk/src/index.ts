72a2f58e88b7276815fbae324226c23356c30c03
Drug <cropflre2333@gmail.com>
2026-07-16 23:14:45 +0800

    feat(sdk): export attachment client (#148)

--- packages/nowen-sdk/src/index.ts
+++ packages/nowen-sdk/src/index.ts
@@ -3,17 +3,21 @@
  *
  * 用法：
  * ```ts
- * import { NowenClient } from "@nowen/sdk";
+ * import { NowenClient, NowenAttachmentClient } from "@nowen/sdk";
  *
- * const client = new NowenClient({
+ * const config = {
  *   baseUrl: "http://localhost:3001",
  *   username: "admin",
  *   password: "admin123",
- * });
+ * };
  *
+ * const client = new NowenClient(config);
+ * const attachments = new NowenAttachmentClient(config);
  * const notebooks = await client.listNotebooks();
  * ```
  */
 
 export { NowenClient } from "./client.js";
+export { NowenAttachmentClient } from "./attachments.js";
 export type * from "./types.js";
+export type * from "./attachments.js";
